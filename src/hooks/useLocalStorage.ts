077697b72528ebadf048a73952d97a118136a6de
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-06-15 11:30:59 +0300

    feat: localstorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,20 @@
+const LocalStorageDataProvider = {
+  getTasks : (keyList:[])=>{
+    let tasks = []
+    for (const key of keyList) {
+      tasks.push(localStorage.getItem(key))
+    }
+    return tasks;
+  },
+  createTask : (task:{}){
+    localStorage.setItem(task?.id, JSON.stringify(task))
+  }
+}
 const useLocalStorage = () => {
- 
+  return {
+    getTasks : (keyList:[]) => LocalStorageDataProvider.getTasks(keyList),
+    createTask : (task:{})=>LocalStorageDataProvider.createTask(task)
+  }
 }
 
 export {
