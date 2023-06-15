41bf1c0df512376f2cd4303752e69b0ff461a40d
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-06-15 11:13:53 +0300

    fixUp: zustand store

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,43 @@
-const useTaskManager = () => {
+type TaskManager = {
+  task: Task|null;
+  title: string|null;
+  action:string;
+}
 
+export const TASK_ACTIONS = {
+  ADD: 'add',
+  SEARCH:"search",
+  DELETE:"delete",
+  UPDATE: "update"
+}
+
+const useTaskManager = (task: Task|null,title:string|null ,action: string) => {
+  if (action == TASK_ACTIONS.ADD) {
+  }
 }
 
 export {
   useTaskManager
 }
+
+type Task = {
+  id: string | null;
+  title: string;
+  description: string;
+}
+
+type TaskStore = {
+  currentTask: Task | null;
+  allTasks: Task[];
+  setAllTasks: (tasks: Task[]) => void;
+};
+
+const taskStore = (set): TaskStore => ({
+  currentTask: null,
+  allTasks: [],
+  setAllTasks: (tasks) => {
+    set((state) => ({
+      userChannels: tasks,
+    }));
+  },
+});
