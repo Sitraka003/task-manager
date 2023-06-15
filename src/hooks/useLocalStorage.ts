993fe089e76b261eda129bc3ce90ea8f56f58665
heiherilala <hei.herilala@gmail.com>
2023-06-15 11:52:32 +0300

    feat: hooks

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,27 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from 'react';
 
-}
+type LocalStorageKey = string;
 
-export {
-  useLocalStorage
-}
+type LocalStorageValue = string | null;
+
+const useLocalStorage = (
+  key: LocalStorageKey,
+  initialValue: LocalStorageValue
+): [LocalStorageValue, (value: LocalStorageValue) => void] => {
+  const [value, setValue] = useState<LocalStorageValue>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? storedValue : initialValue;
+  });
+
+  useEffect(() => {
+    if (value !== null) {
+      localStorage.setItem(key, value);
+    } else {
+      localStorage.removeItem(key);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
