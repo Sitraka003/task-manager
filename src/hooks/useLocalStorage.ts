0841ba6c40dfec7bc94e1090c0b571f6286612df
FaniryKeziah <hei.keziah.2@gmail.com>
2023-06-15 11:49:15 +0300

    feat: add useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,20 @@
-const useLocalStorage = () => {
+import create from 'zustand';
 
-}
+export type Value = {
+  id: number;
+  title: string;
+  setValues: (values: { id: number; title: string }) => void;
+};
+
+const useLocalStorage = create<Value>((set) => ({
+  id: parseInt(localStorage.getItem('id') ?? '0', 10),
+  title: localStorage.getItem('title') ?? '',
+  setValues: (values) => {
+    set(values);
+    localStorage.setItem('id', values.id.toString());
+    localStorage.setItem('title', values.title);
+  },
+}));
 
 export {
   useLocalStorage
